6cdc7d4144c20ff4f40e774ea5d3812daa8241bb
Kévin Vallin <kevin.vallin@ynov.com>
2023-01-11 00:55:21 +0100

    Update Main.kt

--- Machine/Main.kt
+++ Machine/Main.kt
@@ -171,7 +171,125 @@ class CoffeeMachine(
 //    coffeeMachine.showResources()
 //
 //}
+//    //Step 4
+//fun main() {
+//    //stage 4
+//    var water = 400
+//    var milk = 540
+//    var beans = 120
+//    var d_cups = 9
+//    var money = 550
 //
+//    var _water = 250
+//    var _milk = 0
+//    var _beans = 16
+//    var _cost = 4
+//    val espresso = listOf(_water, _milk, _beans, _cost)
+//
+//    _water = 350
+//    _milk = 75
+//    _beans = 20
+//    _cost = 7
+//    val latte = listOf(_water, _milk, _beans, _cost)
+//
+//    _water = 200
+//    _milk = 100
+//    _beans = 12
+//    _cost = 6
+//
+//    val cappuccino = listOf(_water, _milk, _beans, _cost)
+//
+//    fun buy() {
+//        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: \n")
+//        val buyWhat = readln()
+//        val what: List<Int>
+//        when (buyWhat) {
+//            "1" -> what = espresso
+//            "2" -> what = latte
+//            "3" -> what = cappuccino
+//            "back" -> return
+//
+//            else -> return
+//        }
+//        if (what[0] > water) {
+//            println("Sorry, not enough water!")
+//            return
+//        }
+//        if (what[1] > milk) {
+//            println("Sorry, not enough milk!")
+//            return
+//        }
+//
+//        if (what[2] > beans) {
+//            println("Sorry, not enough beans!")
+//            return
+//        }
+//        if (d_cups == 0) {
+//            println("Sorry, not enough cups!")
+//            return
+//        }
+//        water -= what[0]
+//        milk -= what[1]
+//        beans -= what[2]
+//        money += what[3]
+//        d_cups--
+////        println("I have enough resources, making you a coffee!")
+//        println("The coffee machine has:")
+//        println("$water ml of water")
+//        println("$milk ml of milk")
+//        println("$beans g of coffee beans")
+//        println("$d_cups disposable cups" )
+//        println("\$$money of money\n")
+//
+//    }
+//
+//    fun fill() {
+//        println("Write how many ml of water you want to add:")
+//        val w = readln().toInt()
+//        print("Write how many ml of milk you want to add: \n")
+//        val m = readln().toInt()
+//        print("Write how many grams of coffee beans you want to add:\n")
+//        val b = readln().toInt()
+//        print("Write how many disposable cups you want to add:\n")
+//        val c = readln().toInt()
+//
+//        water += w
+//        milk += m
+//        beans += b
+//        d_cups += c
+//
+//        println("The coffee machine has:")
+//        println("$water ml of water")
+//        println("$milk ml of milk")
+//        println("$beans g of coffee beans")
+//        println("$d_cups disposable cups" )
+//        println("\$$money of money\n")
+//    }
+//
+//    fun take() {
+//        println("I gave you \$$money \n")
+//        money = 0
+//        println("The coffee machine has:")
+//        println("$water ml of water")
+//        println("$milk ml of milk")
+//        println("$beans g of coffee beans")
+//        println("$d_cups disposable cups" )
+//        println("\$$money of money")
+//    }
+//
+//    println("The coffee machine has:")
+//    println("$water ml of water")
+//    println("$milk ml of milk")
+//    println("$beans g of coffee beans")
+//    println("$d_cups disposable cups" )
+//    println("\$$money of money\n")
+//    print("Write action (buy, fill, take): \n")
+//    when (scanner.next()) {
+//        "buy" -> buy()
+//        "fill" -> fill()
+//        "take" -> take()
+//    }
+//}
 //    //endregion
 //
 
