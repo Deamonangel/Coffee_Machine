dd793f21d8d69768484d68e8da7671a93c8b7ce0
Kévin Vallin <kevin.vallin@ynov.com>
2023-02-01 13:20:40 +0100

    Update Step03.kt

--- Simple Tic-Tac-Toe/Step03.kt
+++ Simple Tic-Tac-Toe/Step03.kt
@@ -4,7 +4,7 @@ fun main() {
     print("Enter the grid string: ")
     val input = readLine()!! // read the input from the user
     val grid = input.toCharArray() // convert the input to a char array
-    
+
     // print the top border
     println("---------")
 
@@ -23,32 +23,65 @@ fun main() {
 
     // print the bottom border
     println("---------")
-    
-// check for horizontal wins
-for (i in 0..6 step 3) {
-    if (grid[i] == grid[i + 1] && grid[i + 1] == grid[i + 2]) {
-        if (grid[i] == 'X') {
-            println("X wins")
-            return
-        } else if (grid[i] == 'O') {
-            println("O wins")
-            return
+
+    var xCount = 0
+    var oCount = 0
+    var emptyCount = 0
+    var xWin = false
+    var oWin = false
+
+    // check for horizontal wins
+    for (i in 0..6 step 3) {
+        if (grid[i] == grid[i + 1] && grid[i + 1] == grid[i + 2]) {
+            if (grid[i] == 'X') {
+                xWin = true
+            } else if (grid[i] == 'O') {
+                oWin = true
+            }
         }
     }
-}
 
-// check for vertical wins
-for (i in 0..2) {
-    if (grid[i] == grid[i + 3] && grid[i + 3] == grid[i + 6]) {
-        if (grid[i] == 'X') {
-            println("X wins")
-            return
-        } else if (grid[i] == 'O') {
-            println("O wins")
-            return
+    // check for vertical wins
+    for (i in 0..2) {
+        if (grid[i] == grid[i + 3] && grid[i + 3] == grid[i + 6]) {
+            if (grid[i] == 'X') {
+                xWin = true
+            } else if (grid[i] == 'O') {
+                oWin = true
+            }
         }
     }
-}
 
-// check for diagonal wins
+    // check for diagonal wins
+    if ((grid[0] == grid[4] && grid[4] == grid[8]) || (grid[2] == grid[4] && grid[4] == grid[6])) {
+        if (grid[4] == 'X') {
+            xWin = true
+        } else if (grid[4] == 'O') {
+            oWin = true
+        }
+    }
+
+    // count the number of X's, O's, and empty cells
+    for (i in grid.indices) {
+        if (grid[i] == 'X') {
+            xCount++
+        } else if (grid[i] == 'O') {
+            oCount++
+        } else if (grid[i] == '_') {
+            emptyCount++
+        }
+    }
+
+    // determine if the game is impossible, X wins, O wins, draw, or not finished
+    if (xWin && oWin || xCount - oCount > 1 || oCount - xCount > 1) {
+        println("Impossible")
+    } else if (xWin) {
+        println("X wins")
+    } else if (oWin) {
+        println("O wins")
+    } else if (emptyCount > 0) {
+        println("Game not finished")
+    } else {
+        println("Draw")
+    }
 }
